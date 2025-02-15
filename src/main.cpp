a477f56fc20d81ada12728092d02094dc68bc24b
vinceneto <vnetoxd@gmail.com>
2025-02-15 10:05:44 -0400

    + pagina web e esquema eletrico

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <WiFi.h>
 #include <LiquidCrystal_I2C.h>
 
+/* Definições da Rede */
+const char* SSID = "";      // Nome da rede WiFi
+const char* PASSWORD = "";  // Senha da rede WiFi
+
+WiFiServer server(80);
+
+/* Definições do LCD */
 #define LCD_ADDR    0x27
 #define LCD_COLUMNS 16
 #define LCD_ROWS    2
 
-#define SENSOR_FINISH_PIN    9
-#define SENSOR_START_PIN     8
-#define BOTAO_RESET_PIN      7
-
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLUMNS, LCD_ROWS);  // Endereço I2C do LCD 16x2
 
+/* Definições de I/O */
+#define SENSOR_FINISH_PIN    27
+#define SENSOR_START_PIN     26
+#define BOTAO_RESET_PIN      15
+#define LED_STATUS           2
+
+/* Variáveis auxiliares */
 unsigned long startTime;
 unsigned long elapsedTime = 0;
 boolean isRunning = false;
@@ -27,7 +37,7 @@ void startTimer()
 
 void pauseTimer()
 {
-  isRunning = false;
+    isRunning = false;
 }
 
 void updateDisplay() 
@@ -49,8 +59,34 @@ void resetTimer()
     lcd.print("Pressione START");
 }
 
-void setup() {
-    lcd.begin(16,2);   // Inicializa o LCD
+void setup() 
+{
+    Serial.begin(115200);
+    pinMode(LED_STATUS, OUTPUT);
+    pinMode(BOTAO_RESET_PIN, INPUT);
+    pinMode(SENSOR_START_PIN, INPUT);
+    pinMode(SENSOR_FINISH_PIN, INPUT);
+    
+    WiFi.begin(SSID, PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        digitalWrite(LED_STATUS, HIGH);
+        delay(500);
+        Serial.print(".");
+        digitalWrite(LED_STATUS, LOW);
+        delay(250);
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi conectada.");
+    Serial.println("Endereço de IP: ");
+    Serial.println(WiFi.localIP());
+    digitalWrite(LED_STATUS, HIGH);
+ 
+    server.begin();
+
+    lcd.init();
     lcd.backlight();    // Ativa a luz de fundo
     lcd.setCursor(0,0);
     lcd.print("Cronometro:");
@@ -64,6 +100,51 @@ void loop()
     {
         elapsedTime = millis() - startTime;
         updateDisplay();
+    }
+
+    WiFiClient client = server.available();
+
+    if (client) 
+    {
+        String currentLine = "";
+        while (client.connected()) 
+        {
+            if (client.available()) 
+            {
+                char c = client.read();
+                if (c == '\n') 
+                {
+                    if (currentLine.length() == 0) 
+                    {
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        client.println("<!DOCTYPE html><html><head><title>Cronômetro</title></head><body>");
+                        client.println("<script>");
+                        client.println("function refreshPage() {");
+                        client.println("  location.reload(true);");
+                        client.println("}");
+                        client.println("setInterval(refreshPage, 1000);");  // Refresh every 1 second
+                        client.println("</script></head><body>");
+                        client.println("<h1>Cronometro</h1>");
+                        client.println("<p>Tempo: " + String(elapsedTime) + " ms</p>");
+                        client.println("</body></html>");
+                        break;
+                    } 
+                    else 
+                    {
+                        currentLine = "";
+                    }
+                } 
+                else if (c != '\r') 
+                {
+                    currentLine += c;
+                }
+            }
+        }
+        client.stop();
     }
 
     // Sensor para iniciar o cronômetro
